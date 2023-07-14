235c40d85be0366b46d99cbbcd8e921b1bc7674a
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-14 09:32:40 -0700

    Add User App using Jetpack Compose.

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -7,10 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,35 +32,72 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePracticeTheme {
-                var count by remember {
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
                 }
                 Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
                 ) {
-                    Text(
-                        text = count.toString(),
-                        fontSize = 30.sp
-                    )
-                    Button(onClick = {
-                        count++
-                    }) {
-                        Text(text = "Click Me!")
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if (name.isNotBlank()) {
+                                names = names + name
+                                name = ""
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
                     }
+
+                    NameList(names = names)
 
                 }
 
             }
+        }
+    }
+}
+
+
+@Composable
+fun NameList(
+    names: List<String>,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(modifier = modifier) {
+        items(names) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
         }
     }
 }
