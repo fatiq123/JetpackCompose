fbb263ee6ecca9fa850f94cf8fef8df88ebf3a23
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-08 07:19:15 -0700

    ConstraintLayout

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -59,6 +60,9 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.launch
 import java.util.Random
 
@@ -68,12 +72,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-           Lists()
+            ConstraintLayout()
 
         }
     }
 }
 
+
+@Composable
+fun ConstraintLayout() {
+
+    val constraintSet = ConstraintSet {
+        val greenBox = createRefFor("greenBox")
+        val blueBox = createRefFor("blueBox")
+
+        constrain(greenBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        constrain(blueBox) {
+            top.linkTo(parent.top)
+            start.linkTo(greenBox.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        createHorizontalChain(greenBox, blueBox, chainStyle = ChainStyle.Spread)
+    }
+
+    androidx.constraintlayout.compose.ConstraintLayout(
+        constraintSet = constraintSet,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("greenBox")
+        )
+//        Spacer(modifier = Modifier.width(50.dp))
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+                .layoutId("blueBox")
+        )
+    }
+
+}
 
 @Composable
 fun Lists() {
